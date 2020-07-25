9ceb34bd908f4d4d93c0ffbb7c1aa2097623ce97
Waldman200036 <Waldman200036@gmail.com>
2020-07-25 01:00:27 -0500

    Complete

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,6 +1,5 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
@@ -8,18 +7,24 @@ export interface HitCounterProps {
   downstream: lambda.IFunction;
 }
 
-export class HitCounter extends cdk.Construct { 
+export class HitCounter extends cdk.Construct {
 
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
     const table = new dynamodb.Table(this, 'Hits', {
-      partitionKey: { name: 'path', type: 
-        dynamodb.AttributeType.STRING }
+      partitionKey: {
+        name: 'path', type:
+          dynamodb.AttributeType.STRING
+      }
     });
+
+    this.table = table;
 
     this.handler = new lambda.Function(this, 'HitCounterHandler', {
       runtime: lambda.Runtime.NODEJS_10_X,
@@ -29,6 +34,11 @@ export class HitCounter extends cdk.Construct {
         DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
         HITS_TABLE_NAME: table.tableName
       }
-    })
+    });
+    // grant the lambda role read/write permissions to our table
+    table.grantReadWriteData(this.handler);
+
+    // grant the lambda role invoke permissions to the downstream function
+    props.downstream.grantInvoke(this.handler);
   }
 }
